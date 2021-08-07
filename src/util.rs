e68d13212dcb13ae9f273b101ad5c3603454cfd1
Trevor McKay <tm@trmckay.com>
2021-08-06 23:00:02 -0700

    passing tests

--- src/util.rs
+++ src/util.rs
@@ -3,10 +3,21 @@
 #[macro_export]
 macro_rules! parse_int {
     ($t:ty,$s:expr) => {{
-        if $s.len() < 2 && !$s.to_ascii_lowercase().starts_with("0x") {
+        if !$s.to_ascii_lowercase().starts_with("0x") {
             $s.parse()
         } else {
             <$t>::from_str_radix(&$s[2..], 16)
         }
     }};
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::parse_int;
+
+    #[test]
+    fn test_parse_int() {
+        assert_eq!(17, parse_int!(u32, "17").unwrap());
+        assert_eq!(17, parse_int!(i32, "17").unwrap());
+    }
 }
