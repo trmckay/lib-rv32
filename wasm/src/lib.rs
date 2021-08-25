61e581c01147491af377300b762df767bac1a1c0
Trevor McKay <tm@trmckay.com>
2021-08-24 22:08:39 -0700

    update wasm bindings

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,15 +1,23 @@
+use log::{info, LevelFilter};
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
-use serde::{Serialize, Deserialize};
 
-use lib_rv32_mcu::*;
 use lib_rv32_asm::assemble_program;
+use lib_rv32_common::constants::*;
 use lib_rv32_isa::exec_one;
+use lib_rv32_mcu::*;
+
+mod logger;
+use logger::*;
 
 pub const DEFAULT_MEM_SIZE: usize = 1024;
+
+static LOGGER: Logger = Logger;
 
 #[wasm_bindgen]
 pub struct State {
     mcu: Mcu,
+    text_size: usize,
 }
 
 #[wasm_bindgen]
@@ -22,23 +30,64 @@ pub struct Program {
 impl State {
     /// Initiate the MCU state.
     pub fn new() -> Self {
-        State {
-            mcu: Mcu::new(DEFAULT_MEM_SIZE),
-        }
+        log::set_logger(&LOGGER)
+            .map(|()| log::set_max_level(LevelFilter::Info))
+            .unwrap();
+
+        let mcu = Mcu::new(DEFAULT_MEM_SIZE);
+        info!(
+            "Initialized RISC-V rv32i MCU with {}k memory.",
+            mcu.mem.size / 1024
+        );
+
+        State { mcu, text_size: 0 }
     }
 
     /// Program the MCU with a string program.
-    pub fn program(&mut self, program: JsValue) {
-        let program: String = program.into_serde().unwrap();
-        let words = assemble_program(&program).unwrap();
+    pub fn assemble(&mut self, program: String) {
+        let program = program.replace("\\n", "\n");
 
-        for (wa, wd) in words.iter().enumerate() {
-            self.mcu.mem.write_word(wa as u32, *wd).unwrap();
+        let words = assemble_program(&program);
+
+        if words.is_err() {
+            info!("Assembler error: {:?}", words);
+            return;
+        } else {
+            info!("Successfully assembled program.");
+            info!(" ");
+            let words = words.unwrap();
+            self.mcu.mem.program_words(&words).unwrap();
+            self.text_size = words.len() * 4;
         }
     }
 
-    /// Advance the MCU by one instruction.
-    pub fn exec_one(&mut self) {
-        exec_one(&mut self.mcu.pc, &mut self.mcu.mem, &mut self.mcu.rf);
+    pub fn run(&mut self) {
+        while self.mcu.pc < self.text_size as u32 {
+            if let Err(why) = exec_one(&mut self.mcu.pc, &mut self.mcu.mem, &mut self.mcu.rf) {
+                info!("MCU runtime error: {:?}", why);
+                return;
+            }
+        }
+
+        info!("Program complete.");
+    }
+
+    pub fn get_text(&self) -> String {
+        let mut text = String::new();
+        for wa in 0..self.text_size / 4 {
+            text += &format!("{:08x}\n", self.mcu.mem.fetch((wa as u32) * 4).unwrap());
+        }
+        text
+    }
+
+    pub fn get_state(&self) -> String {
+        let mut state = String::new();
+
+        for i in 0..32 {
+            let val = self.mcu.rf.read(i as u8).unwrap();
+            state += &format!("{:4} = 0x{:08x} ({})\n", REG_NAMES[i], val, val as i32);
+        }
+
+        state
     }
 }
---
+++ wasm/src/logger.rs
@@ -0,0 +1,24 @@
+pub use log::{Level, LevelFilter, Metadata, Record};
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+pub struct Logger;
+
+impl log::Log for Logger {
+    fn enabled(&self, metadata: &Metadata) -> bool {
+        metadata.level() <= Level::Info
+    }
+
+    fn log(&self, record: &Record) {
+        if self.enabled(record.metadata()) {
+            log(&format!("{}", record.args()));
+        }
+    }
+
+    fn flush(&self) {}
+}
